f4f443246e2c84adaec038b5a298a41bde3cadaa
Eugene Husnullin <ehusnullin80@gmail.com>
2014-12-08 13:59:22 +0200

    fix org.hibernate.TypeMismatchException: Provided id of the wrong type

--- src/main/java/ru/gm/munic/service/processing/LowService.java
+++ src/main/java/ru/gm/munic/service/processing/LowService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -117,79 +116,81 @@ public class LowService {
 			}
 
 			// check distance
-			try {
-				if (municData.hasMileage()) {
-					if (municData.getMunicDataMdi().getMdiObdMileage() != 0) {
-						@SuppressWarnings("unchecked")
-						List<MunicData> prevList = session.createCriteria(MunicData.class)
-								.add(Restrictions.eq("asset", municData.getAsset()))
-								.add(Restrictions.lt("recordedAt", municData.getRecordedAt()))
-								.addOrder(Order.desc("recordedAt"))
-								.setMaxResults(1)
-								.createCriteria("municDataMdi")
-									.add(Restrictions.isNotNull("mdiObdMileage"))
-								.list();
-						
-						if (prevList.size() == 0) {
-							logger.error("Unexpecting behavior while quering municdata for previvouse trip.");
-						} else {
-							MunicData prevMD = prevList.get(0);
-							Integer obdDistance = municData.getMunicDataMdi().getMdiObdMileage() - prevMD.getMunicDataMdi().getMdiObdMileage();
-							
-							// calculate distance by gps coordinates
-							@SuppressWarnings("unchecked")
-							List<MunicData> listOfGps = session.createCriteria(MunicData.class)
-									.add(Restrictions.eq("asset", municData.getAsset()))
-									.add(Restrictions.le("recordedAt", municData.getRecordedAt()))
-									.add(Restrictions.gt("recordedAt", prevMD.getRecordedAt()))
-									.add(Restrictions.isNotNull("lat"))
-									.add(Restrictions.isNotNull("lon"))
-									.addOrder(Order.asc("recordedAt"))
-									.list();
-							
-							int gpsDistance = 0;
-							double prevLat = listOfGps.get(0).getLat();
-							double prevLon = listOfGps.get(0).getLon();
-							for (int i = 1; i < listOfGps.size(); i++) {
-								gpsDistance += calcDistance(prevLat, prevLon, listOfGps.get(i).getLat(), listOfGps.get(i).getLon());
-							}
-							
-							if (gpsDistance != 0) {
-								double percent = Math.abs(obdDistance * 100 / gpsDistance);
-								if (percent > maxPercent) {
-									TopBlockAlert alert = new TopBlockAlert();
-									alert.setTopAuto(topAuto);
-									alert.setAddDate(new Date());
-									alert.setStatus(2);
-									alert.setAlertType(1);
-									
-									JSONObject jsonAlert = new JSONObject();
-									jsonAlert.put("percent", percent);
-									jsonAlert.put("gpsDistance", gpsDistance);
-									jsonAlert.put("obdDistance", obdDistance);
-									if (municData.getLat() != null && municData.getLon() != null) {
-										jsonAlert.put("lat",municData.getLat());
-										jsonAlert.put("lot",municData.getLon());
-									}
-									
-									alert.setMessage(jsonAlert.toString());
-									session.save(alert);
-								}
-							}
-						}
-						
-					}
-				}
-			}catch(Exception e) {
-				logger.error(e.toString());
-			}
+//			try {
+//				if (municData.hasMileage()) {
+//					if (municData.getMunicDataMdi().getMdiObdMileage() != 0) {
+//						@SuppressWarnings("unchecked")
+//						List<MunicData> prevList = session.createCriteria(MunicData.class)
+//								.add(Restrictions.eq("asset", municData.getAsset()))
+//								.add(Restrictions.lt("recordedAt", municData.getRecordedAt()))
+//								.addOrder(Order.desc("recordedAt"))
+//								.setMaxResults(1)
+//								.createCriteria("municDataMdi")
+//									.add(Restrictions.isNotNull("mdiObdMileage"))
+//								.list();
+//						
+//						if (prevList.size() == 0) {
+//							logger.error("Unexpecting behavior while quering municdata for previvouse trip.");
+//						} else {
+//							MunicData prevMD = prevList.get(0);
+//							Integer obdDistance = municData.getMunicDataMdi().getMdiObdMileage() - prevMD.getMunicDataMdi().getMdiObdMileage();
+//							
+//							// calculate distance by gps coordinates
+//							@SuppressWarnings("unchecked")
+//							List<MunicData> listOfGps = session.createCriteria(MunicData.class)
+//									.add(Restrictions.eq("asset", municData.getAsset()))
+//									.add(Restrictions.le("recordedAt", municData.getRecordedAt()))
+//									.add(Restrictions.gt("recordedAt", prevMD.getRecordedAt()))
+//									.add(Restrictions.isNotNull("lat"))
+//									.add(Restrictions.isNotNull("lon"))
+//									.addOrder(Order.asc("recordedAt"))
+//									.list();
+//							
+//							int gpsDistance = 0;
+//							double prevLat = listOfGps.get(0).getLat();
+//							double prevLon = listOfGps.get(0).getLon();
+//							for (int i = 1; i < listOfGps.size(); i++) {
+//								gpsDistance += calcDistance(prevLat, prevLon, listOfGps.get(i).getLat(), listOfGps.get(i).getLon());
+//							}
+//							
+//							if (gpsDistance != 0) {
+//								double percent = Math.abs(obdDistance * 100 / gpsDistance);
+//								if (percent > maxPercent) {
+//									TopBlockAlert alert = new TopBlockAlert();
+//									alert.setTopAuto(topAuto);
+//									alert.setAddDate(new Date());
+//									alert.setStatus(2);
+//									alert.setAlertType(1);
+//									
+//									JSONObject jsonAlert = new JSONObject();
+//									jsonAlert.put("percent", percent);
+//									jsonAlert.put("gpsDistance", gpsDistance);
+//									jsonAlert.put("obdDistance", obdDistance);
+//									if (municData.getLat() != null && municData.getLon() != null) {
+//										jsonAlert.put("lat",municData.getLat());
+//										jsonAlert.put("lot",municData.getLon());
+//									}
+//									
+//									alert.setMessage(jsonAlert.toString());
+//									session.save(alert);
+//								}
+//							}
+//						}
+//						
+//					}
+//				}
+//			}catch(Exception e) {
+//				logger.error(e.toString());
+//			}
 		}
+
 		session.save(municData);
 
 		municData.getMunicItemRawData().setProcessed(true);
 		session.update(municData.getMunicItemRawData());
 	}
 	
+	@SuppressWarnings("unused")
 	private double calcDistance(double lat1, double lon1, double lat2, double lon2) {
 		long radius = 6371L;
 

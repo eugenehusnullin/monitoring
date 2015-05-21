cfa09dc8d179241db6a369e74ffa78df9b56d40f
echo <goalinternet@gmail.com>
2015-05-21 19:24:31 +0300

    nis protocol. using mvc.

---
+++ src/main/java/monitoring/handler/cityguide/CityGuideHandler.java
@@ -0,0 +1,66 @@
+package monitoring.handler.cityguide;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+import monitoring.domain.Message;
+import monitoring.handler.Handler;
+import monitoring.handler.HandlerStrategy;
+import monitoring.protocol.nis.NisMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import utils.HttpUtils;
+
+public class CityGuideHandler implements Handler {
+	private static final Logger logger = LoggerFactory.getLogger(CityGuideHandler.class);
+
+	private String url;
+
+	@Override
+	public void handle(Message message, HandlerStrategy strategy) {
+		NisMessage m = (NisMessage) message;
+
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
+
+			Element pointsElement = doc.createElement("points");
+			pointsElement.setAttribute("id", Long.toString(m.getTerminalId()));
+			doc.appendChild(pointsElement);
+
+			Element pointElement = doc.createElement("points");
+			pointElement.setAttribute("speed", Double.toString(m.getSpeed()));
+			pointElement.setAttribute("lat", Double.toString(m.getLat()));
+			pointElement.setAttribute("lon", Double.toString(m.getLon()));
+			pointElement.setAttribute("isotime", parseToIsoTime(m.getTime()));
+			pointsElement.appendChild(pointElement);
+
+			HttpUtils.postDocumentOverHttp(doc, url, logger);
+		} catch (ParserConfigurationException | IOException | TransformerException
+				| TransformerFactoryConfigurationError e) {
+			logger.error("CityGuideHandler error.", e);
+		}
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	private String parseToIsoTime(Date time) {
+		// 2014-10-23T11:01:57+0300
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+		return df.format(time);
+	}
+}
---
+++ src/main/java/monitoring/protocol/nis/NisMessage.java
@@ -0,0 +1,55 @@
+package monitoring.protocol.nis;
+
+import java.util.Date;
+
+import monitoring.domain.Message;
+
+public class NisMessage implements Message {
+	private long terminalId;
+	private double lat;
+	private double lon;
+	private double speed;
+	private Date time;
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	@Override
+	public long getTerminalId() {
+		return terminalId;
+	}
+
+	public void setTerminalId(long terminalId) {
+		this.terminalId = terminalId;
+	}
+
+}

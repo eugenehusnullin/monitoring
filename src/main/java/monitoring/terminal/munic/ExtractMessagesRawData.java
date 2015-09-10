38a645f002a5de3865cc821a2a4435b459d00c79
echo <goalinternet@gmail.com>
2015-09-10 20:09:34 +0300

    Munic. Sort messages before processing.

--- src/main/java/monitoring/terminal/munic/ExtractMessagesRawData.java
+++ src/main/java/monitoring/terminal/munic/ExtractMessagesRawData.java
@@ -1,11 +1,12 @@
 package monitoring.terminal.munic;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
-
-import monitoring.handler.Handler;
-import monitoring.handler.HandlerStrategy;
+import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,12 +14,17 @@ import org.json.JSONTokener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import monitoring.handler.Handler;
+import monitoring.handler.HandlerStrategy;
+
 class ExtractMessagesRawData implements RawHandler {
 	private static final Logger logger = LoggerFactory.getLogger(ExtractMessagesRawData.class);
 
 	private List<Handler> handlers;
 	private HandlerStrategy strategy;
 	private MunicMessageFactory municMessageFactory;
+
+	private Map<Long, State> stateMap = new HashMap<Long, State>();
 
 	@Override
 	public void procces(String message) {
@@ -31,9 +37,22 @@ class ExtractMessagesRawData implements RawHandler {
 	private void proccesMessage(String message) {
 		JSONTokener jsonTokener = new JSONTokener(message);
 		JSONArray jsonArray = (JSONArray) jsonTokener.nextValue();
+		List<MunicMessage> messagesList = new ArrayList<>();
+
+		// init objects from json
 		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject jsonObject = jsonArray.getJSONObject(i);
 			MunicMessage municMessage = municMessageFactory.createMessage(jsonObject);
+			messagesList.add(municMessage);
+		}
+
+		// sort by recorded time
+		messagesList = messagesList.stream().sorted((e1, e2) -> e1.getRecordedAt().compareTo(e2.getRecordedAt()))
+				.collect(Collectors.toList());
+
+		// process messages
+		for (MunicMessage municMessage : messagesList) {
+			stateProof(municMessage);
 			if (municMessage != null) {
 				for (Handler handler : handlers) {
 					handler.handle(municMessage, strategy);
@@ -100,6 +119,41 @@ class ExtractMessagesRawData implements RawHandler {
 
 	public void setMunicMessageFactory(MunicMessageFactory municMessageFactory) {
 		this.municMessageFactory = municMessageFactory;
+	}
+
+	private void stateProof(MunicMessage municMessage) {
+		if (municMessage.isTrackEvent()) {
+			synchronized (stateMap) {
+				State state = stateMap.get(municMessage.getTerminalId());
+				if (state == null) {
+					state = new State();
+					stateMap.put(municMessage.getTerminalId(), state);
+				}
+
+				// ignition
+				if (municMessage.getDioIgnition() != null) {
+					state.setIgnition(municMessage.getDioIgnition());
+				} else {
+					Boolean ignitionState = state.getIgnition();
+					if (ignitionState != null) {
+						municMessage.setDioIgnition(ignitionState);
+					}
+				}
+
+				// gprmc valid
+				if (municMessage.getGprmcValid() != null) {
+					state.setGpsValid(municMessage.getGprmcValid());
+				} else {
+					Boolean gprmcValid = state.getGpsValid();
+					if (gprmcValid != null) {
+						municMessage.setGprmcValid(gprmcValid);
+					}
+				}
+
+				// notify all threads
+				stateMap.notifyAll();
+			}
+		}
 	}
 
 }
--- src/main/java/monitoring/terminal/munic/MunicMessageFactory.java
+++ src/main/java/monitoring/terminal/munic/MunicMessageFactory.java
@@ -1,8 +1,6 @@
 package monitoring.terminal.munic;
 
 import java.text.ParseException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,14 +10,10 @@ import org.slf4j.LoggerFactory;
 public class MunicMessageFactory {
 	private static final Logger logger = LoggerFactory.getLogger(MunicMessageFactory.class);
 
-	private Map<Long, State> stateMap = new HashMap<Long, State>();
-
 	public MunicMessage createMessage(JSONObject jsonObject) {
 		MunicMessage municMessage = new MunicMessage();
 		try {
 			municMessage.setJsonMessage(jsonObject);
-			stateProof(municMessage);
-
 		} catch (JSONException | ParseException e) {
 			logger.error(e.toString());
 			return null;
@@ -27,38 +21,4 @@ public class MunicMessageFactory {
 		return municMessage;
 	}
 
-	private void stateProof(MunicMessage municMessage) {
-		if (municMessage.isTrackEvent()) {
-			synchronized (stateMap) {
-				State state = stateMap.get(municMessage.getTerminalId());
-				if (state == null) {
-					state = new State();
-					stateMap.put(municMessage.getTerminalId(), state);
-				}
-
-				// ignition
-				if (municMessage.getDioIgnition() != null) {
-					state.setIgnition(municMessage.getDioIgnition());
-				} else {
-					Boolean ignitionState = state.getIgnition();
-					if (ignitionState != null) {
-						municMessage.setDioIgnition(ignitionState);
-					}
-				}
-
-				// gprmc valid
-				if (municMessage.getGprmcValid() != null) {
-					state.setGpsValid(municMessage.getGprmcValid());
-				} else {
-					Boolean gprmcValid = state.getGpsValid();
-					if (gprmcValid != null) {
-						municMessage.setGprmcValid(gprmcValid);
-					}
-				}
-
-				// notify all threads
-				stateMap.notifyAll();
-			}
-		}
-	}
 }
